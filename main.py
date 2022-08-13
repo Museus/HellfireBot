2d64538000f6dd63791c78ea3008be5b60f8790e
Randy <randy5yudi2zhang4@gmail.com>
2022-08-12 23:55:32 -0700

    a million changes, including full boon aliasing and weapon aspects

--- main.py
+++ main.py
@@ -11,14 +11,6 @@ import misc
 
 client = commands.Bot(command_prefix=['h!'], case_insensitive=True)
 
-boons_info = {}
-f = open('./booninfo.txt', 'r', encoding='utf8')
-while boon := f.readline().strip():
-    type, boon = boon.split(' ', 1)
-    boons_info[boon] = {'type': type, 'desc': f.readline().strip(), 'stat': f.readline().strip(),
-                        'rarities': f.readline().strip().split(' '), 'levels': f.readline().strip().split(' '),
-                        'icon': f.readline()}
-
 
 @client.event
 async def on_ready():
@@ -29,7 +21,10 @@ async def on_ready():
 @client.command(aliases=['b'])
 async def boon(ctx, *args) -> None:
     name, rarity, level = misc.parse_boon(args)
-    info = boons_info[name.lower()]
+    if not name:
+        print('bruh')
+        return
+    info = misc.boons_info[name]
     if rarity == 'heroic' and len(info['rarities']) == 3:
         rarity = 'epic'
     value = info['rarities'][misc.rarities[rarity] - 1]
@@ -55,36 +50,31 @@ async def boon(ctx, *args) -> None:
     embed = discord.Embed(
         title=f'**{string.capwords(name)}** ({level_display})',
         description=f'{info["desc"]}\n▸{misc.parse_stat(info["stat"], value)}')
-    if info['type'] in ['legendary', 'duo']:
-        embed.title = f'**{string.capwords(name)}**'
     if info['type'] in ['legendary']:
         embed.color = 0xFFD511
     elif info['type'] in ['duo']:
         embed.color = 0xD1FF18
     else:
-        embed.color = misc.rarity_colors[misc.rarities[rarity] - 1]
+        embed.color = misc.rarity_embed_colors[misc.rarities[rarity] - 1]
     embed.set_thumbnail(url=info['icon'])
     await ctx.reply(embed=embed, mention_author=False)
 
 
 @client.command(aliases=['ps'])
 async def pomscaling(ctx, *args) -> None:
-    def color_int_str(color_int: int) -> str:
-        color = hex(color_int)
-        return f'#{color.split("x")[1]:0>6}'
     level = 10
     if args[len(args) - 1].isdigit():
         level = int(args[len(args) - 1])
         args = args[0: len(args) - 1]
     name, _, _ = misc.parse_boon(args)
-    info = boons_info[name.lower()]
+    info = misc.boons_info[name]
     values = info['rarities'].copy()
     for rarity, value in enumerate(values):
         if '-' in value:
             value = value.split('-')
-            values[rarity] = (float(info['rarities'][0]) * float(value[0]) + float(info['rarities'][0]) * float(value[1])) / 2
+            values[rarity] = [float(info['rarities'][0]) * float(v) for v in value]
         else:
-            values[rarity] = float(value)
+            values[rarity] = [float(value)]
     pom = 0
     rarity_damages = []
     for i in range(len(values)):
@@ -93,11 +83,21 @@ async def pomscaling(ctx, *args) -> None:
         pom = min(pom, len(info['levels']) - 1)
         for rarity, value in enumerate(values):
             rarity_damages[rarity].append(value)
-            values[rarity] += float(info['levels'][pom])
+            new_value = values[rarity].copy()
+            new_value[0] += float(info['levels'][pom])
+            if len(values[rarity]) == 2:
+                new_value[1] += float(info['levels'][pom])
+            values[rarity] = new_value
         pom += 1
     plt.clf()
+    level_axis = list(range(1, level + 1))
     for rarity, damages in enumerate(rarity_damages):
-        plt.plot(list(range(1, level + 1)), damages, color=color_int_str(misc.rarity_colors[rarity]))
+        lower = [d[0] for d in damages]
+        plt.plot(level_axis, lower, color=misc.rarity_graph_colors[rarity])
+        if len(damages[0]) == 2:
+            upper = [d[1] for d in damages]
+            plt.plot(level_axis, upper, color=misc.rarity_graph_colors[rarity])
+            plt.fill_between(list(range(1, level + 1)), lower, upper, color=misc.rarity_graph_colors[rarity], alpha=0.5)
     plt.xlabel('Level')
     plt.ylabel(info['stat'].split(':')[0])
     plt.ylim(ymin=0)
@@ -111,6 +111,24 @@ async def pomscaling(ctx, *args) -> None:
     embed.set_image(url='attachment://image.png')
     await ctx.reply(file=file, embed=embed, mention_author=False)
     os.remove('output.png')
+
+
+@client.command(aliases=['a'])
+async def aspect(ctx, *args):
+    name, level = misc.parse_aspect(args)
+    if not name:
+        print('bruh')
+        return
+    info = misc.aspects_info[name]
+    value = [int(info['levels'][level - 1])]
+    embed = discord.Embed(
+        title=f'**Aspect of {string.capwords(name)}** (Lv. {level})',
+        description=f'{info["desc"]}\n▸{misc.parse_stat(info["stat"], value)}',
+        color=misc.rarity_embed_colors[level - 1]
+    )
+    embed.set_footer(text=info['flavor'])
+    embed.set_thumbnail(url=info['icon'])
+    await ctx.reply(embed=embed, mention_author=False)
 
 
 async def reply(ctx, message, mention=False):
--- misc.py
+++ misc.py
@@ -1,7 +1,59 @@
 import re
 
+boons_info = {}
+aspects_info = {}
 rarities = {'common': 1, 'rare': 2, 'epic': 3, 'heroic': 4}
-rarity_colors = [0x7D7D7D, 0x0083F3, 0x9500F6, 0xFF1C10, 0xFFD511, 0xD1FF18, 0x8FFF18]
+rarity_graph_colors = ['#7D7D7D', '#0083F3', '#9500F6', '#FF1C10', '#FFD511']
+rarity_embed_colors = [0xFFFFFF, 0x0083F3, 0x9500F6, 0xFF1C10, 0xFFD511, 0xD1FF18, 0x8FFF18]
+
+god_cores = {'zeus': {}, 'poseidon': {}, 'athena': {}, 'aphrodite': {}, 'artemis': {},
+             'ares': {}, 'dionysus': {}, 'demeter': {}, 'hermes': {}, 'duos': None}
+core_aliases = {}
+misc_aliases = {}
+aspect_aliases = {}
+
+for god in god_cores.keys():
+    f = open(f'files/{god}.txt', 'r', encoding='utf8')
+    while boon := f.readline().strip():
+        type, boon = boon.split(' ', 1)
+        if type in ['attack', 'special', 'cast', 'flare', 'dash', 'call', 'status', 'revenge', 'legendary']:
+            god_cores[god][type] = boon
+        boons_info[boon] = {'type': type, 'desc': f.readline().strip(), 'stat': f.readline().strip(),
+                            'rarities': f.readline().strip().split(' '), 'levels': f.readline().strip().split(' '),
+                            'icon': f.readline().strip()}
+
+f = open(f'files/aspects.txt', 'r', encoding='utf8')
+while aspect := f.readline().strip():
+    aspects_info[aspect] = {'desc': f.readline().strip(), 'stat': f.readline().strip(),
+                            'levels': f.readline().strip().split(' '), 'flavor': f.readline().strip(),
+                            'icon': f.readline().strip()}
+
+f = open('files/corealiases.txt', 'r', encoding='utf8')
+while name := f.readline().strip():
+    aliases = f.readline().strip().split(', ')
+    if aliases[0]:
+        for alias in aliases:
+            if alias in core_aliases:
+                print(f'duplicate alias: {alias}')
+            core_aliases[alias] = name
+
+f = open('files/boonaliases.txt', 'r', encoding='utf8')
+while name := f.readline().strip():
+    aliases = f.readline().strip().split(', ')
+    if aliases[0]:
+        for alias in aliases:
+            if alias in misc_aliases:
+                print(f'duplicate alias: {alias}')
+            misc_aliases[alias] = name
+
+f = open('files/aspectaliases.txt', 'r', encoding='utf8')
+while name := f.readline().strip():
+    aliases = f.readline().strip().split(', ')
+    if aliases[0]:
+        for alias in aliases:
+            if alias in aspect_aliases:
+                print(f'duplicate alias: {alias}')
+            aspect_aliases[alias] = name
 
 
 def parse_boon(input: [str]) -> (str, str, int):
@@ -14,23 +66,56 @@ def parse_boon(input: [str]) -> (str, str, int):
     if input[len(input) - 1] in rarities.keys():
         rarity = input[len(input) - 1]
         input = input[0: len(input) - 1]
-    name = ' '.join(input)
-    return name, rarity, level
+    boon_name = fuzzy_boon(input)
+    return boon_name, rarity, level
 
 
-def adjust_boon_type(info: {}, name: str, rarity: str, level: int) -> (str, str, int):
+def parse_aspect(input: [str]) -> (str, int):
+    input = [s.lower() for s in input]
+    level = 5
+    if input[len(input) - 1].isdigit():
+        level = int(input[len(input) - 1])
+        input = input[0: len(input) - 1]
+    aspect_name = ' '.join(input)
+    if aspect_name in aspects_info:
+        return aspect_name, level
+    if aspect_name in aspect_aliases and aspect_aliases[aspect_name] in aspects_info:
+        return aspect_aliases[aspect_name], level
+    return '', level
+
+
+def fuzzy_boon(input: [str]) -> str:
+    boon_name = ' '.join(input)
+    if boon_name in boons_info:
+        return boon_name
+    if boon_name in misc_aliases and misc_aliases[boon_name] in boons_info:
+        return misc_aliases[boon_name]
+    for index, word in enumerate(input):
+        if word in core_aliases:
+            input[index] = core_aliases[word]
+    if len(input) >= 2:
+        if input[0] in god_cores.keys() and input[1] in god_cores[input[0]].keys():
+            return god_cores[input[0]][input[1]]
+        if input[1] in god_cores.keys() and input[0] in god_cores[input[1]].keys():
+            return god_cores[input[1]][input[0]]
+    if ' '.join(input) in boons_info:
+        return ' '.join(input)
+    return ''
+
+
+def adjust_boon_type(info: {}, boon_name: str, rarity: str, level: int) -> (str, str, int):
     if info['type'] in ['legendary', 'duo']:
-        output = f'**{info["type"].upper()}** {name.upper()}\n'
+        output = f'**{info["type"].upper()}** {boon_name.upper()}\n'
         rarity = 'common'
         level = 1
     else:
         if len(info['rarities']) == 3 and rarity == 'heroic':
             rarity = 'epic'
         if info['levels'][0] == '0':
-            output = f'**{rarity.upper()}** {name.upper()}\n'
+            output = f'**{rarity.upper()}** {boon_name.upper()}\n'
             level = 1
         else:
-            output = f'**{rarity.upper()}** {name.upper()} LV.{level}\n'
+            output = f'**{rarity.upper()}** {boon_name.upper()} LV.{level}\n'
     return output, rarity, level
 
 
@@ -41,15 +126,18 @@ def parse_stat(stat_line: str, value: [float]) -> str:
         return stat_line
     rounded = 's' not in replace and 'x' not in replace
     if len(value) == 2:
-        value = f'{int(value[0] + 0.5)} - {int(value[1] + 0.5)}' if rounded else f'{round(value[0], 2)} - {round(value[1], 2)}'
+        value = f'{int(value[0] + 0.5)} - {int(value[1] + 0.5)}' if rounded \
+            else f'{round(value[0], 2)} - {round(value[1], 2)}'
     else:
         value = int(value[0] + 0.5) if rounded else round(value[0], 2)
     if '+' in replace:
         value = f'+{value}'
+    if '-' in replace:
+        value = f'-{value}'
     if '%' in replace:
         value = f'{value}%'
     if 's' in replace:
-        value = f'{value} Sec'
+        value = f'{value} Sec.'
     if 'x' in replace:
         value = f'{value}x'
     stat = re.sub(r'{.*}', f'**{value}**', stat_line)
